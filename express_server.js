8d5f527be1b1a552292783379248ccdf57ade648
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-14 02:10:40 +0000

    Receive to database

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let sol = Math.random().toString(16).slice(2, 8);
+  return sol;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,6 +15,7 @@ const urlDatabase = {
 };
 
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.send("hello");
 });
@@ -23,11 +29,17 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  }
   res.render("urls_show", templateVars);
 });
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.listen(PORT, () => {
   console.log(`Example app lisenting on port ${PORT}!`);
 });
