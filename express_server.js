c2c292efee75a30197225ddac8e0b0ccb2bec3e9
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-14 21:03:34 +0000

    Add delete function

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
-
+app.use(morgan('dev'));
 const generateRandomString = function() {
   let sol = Math.random().toString(16).slice(2, 8);
   return sol;
@@ -46,6 +47,12 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app lisenting on port ${PORT}!`);
 });
